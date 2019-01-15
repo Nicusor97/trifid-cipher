fe715151ddd31be31de89375b53ce700fdb2bbad
Nicusor97 <nicolaepicatureanu@gmail.com>
2019-01-15 21:17:19 +0200

    First commit

---
+++ trifid_cipher.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+import Polybius
+import optparse
+import codecs
+import sys
+import re
+
+__author__ = 'Picatureanu Nicusor'
+__version__ = '1.0'
+
+
+def transform(data):
+
+    data = re.sub(r'[\D]+', '', data)
+
+    return data[::2] + data[1::2]
+
+
+def transform_back(data):
+
+    data = re.sub(r'[\D]+', '', data)
+    numbers = ''
+
+    a, b = (data[:len(data) / 2], data[len(data) / 2:])
+
+    for i in range(len(a)):
+        numbers += a[i] + b[i]
+
+    return numbers
+
+
+if __name__ == '__main__':
+    parser = optparse.OptionParser(version=__version__,
+                                   usage='Usage: %prog [options] [args]',
+                                   description='Trifid cipher is a cipher which combines the Polybius square with transposition, and uses fractionation to achieve diffusion.')
+    parser.add_option('-d', dest='decrypt', action='store_true', default=False)
+    parser.add_option('-k', dest='key', action='store', default='', type='string')
+    parser.add_option('-V', dest='verbose', action='store_true', default=False)
+    parser.add_option('-f', dest='file', action='store', default=False)
+
+    (options, args) = parser.parse_args()
+
+    array = Polybius.generate_array(key=options.key)
+
+    Polybius.display_array(array)
+
+    if options.file:
+        print ''
+        print ' IN> ' + options.file
+
+        try:
+            file_stream = codecs.open(options.file, 'r', 'dbcs')
+            file_output = codecs.open('_' + options.file, 'w', 'dbcs')
+            for line in file_stream:
+                if options.decrypt:
+                    file_output.write(Polybius.decode(
+                        transform_back(
+                            Polybius.encode(line, array)), array))
+                else:
+                    file_output.write(Polybius.decode(
+                        transform(
+                            Polybius.encode(line, array)), array))
+            file_stream.close()
+            file_output.close()
+        except IOError as e:
+            print '\nI/O error({0}): {1}'.format(e.errno, e.strerror)
+            sys.exit(1)
+
+        print 'OUT> ' + '_' + options.file
+
+    else:
+        while 1:
+            print ''
+            try:
+                text = raw_input(' IN> ').decode(sys.stdin.encoding)
+                if options.decrypt:
+                    if options.verbose:
+                        print '  1: %s ' % Polybius.format_cipher(
+                            Polybius.encode(text, array))
+                        print '  2: %s ' % Polybius.format_cipher(
+                            transform_back(
+                                Polybius.encode(text, array)))
+                    print 'OUT> ' + Polybius.decode(
+                        transform_back(
+                            Polybius.encode(text, array)), array)
+                else:
+                    if options.verbose:
+                        print '  1: %s  ' % Polybius.format_cipher(
+                            Polybius.encode(text, array))
+                        print '  2: %s  ' % Polybius.format_cipher(
+                            transform_back(
+                                Polybius.encode(text, array)))
+                    print 'OUT> ' + Polybius.decode(
+                        transform(
+                            Polybius.encode(text, array)), array)
+            except (SystemExit, KeyboardInterrupt):
+sys.exit(0)
